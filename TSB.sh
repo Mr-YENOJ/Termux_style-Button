e34c681c082c156efb9f389194aceba4040a6d67
Inderveerjoney <62381493+Inderveerjoney@users.noreply.github.com>
2020-04-02 12:50:30 +0530

    Create TSB.sh

---
+++ TSB.sh
@@ -0,0 +1,239 @@
+echo " "
+echo " "
+echo " "
+echo " "
+figlet Termux_S_B | toilet -f term -F gay
+echo " "
+echo " "
+echo " "
+echo " "
+bash pkg.txt
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+cd /data/data/com.termux/files/usr/etc
+ls
+rm bash.bashrc
+cd Termux_style-Button
+cp bash.bashrc /data/data/com.termux/files/usr/etc
+cd
+clear
+cd Termux_style-Button
+ls
+cp musi.mp3 /sdcard
+ls
+cd
+clear
+#ab ho jaya ga tention not
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#abaa tu chutiya ha kya jab bol dea ho jaya ga tho ho jaya ga na
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#matlab nhi mano ga 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#phala ya padho fir batyu ga 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ya mana apne bandi   ka lea banya ha but ap padh lo 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#matlab padh kar raho ga nhi mano ga 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#koi nhi padh lo 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Ashiq hu dil ka muje jeena mat sikha
+#Mu mai le lund muje chodna mat sikha
+#Tuta mera dil usse jodna mat sikha
+#Mu mai le lund muje chodna mat sikha
+
+#Tere liye mene mera ghar-daar chhoda
+#Der ke sath piya mene jeera soda
+#Jese matke mai paani bharta thoda thoda
+#Tujhya aaicha ghoda aata pakad maza lauda
+
+#Mere pyaare dil ko tune dag diya
+#Mere soye lund ko tune jaga diya
+#Teri yaad mai din bhar kitna hilau
+#Kitne sare dosto ko mai daaru pilau
+#Jee karda mere dosto se milau
+#Un sab se milake tuje paani pilau
+
+
+
+
+
+
+
+#kasa laga 
+
+#nice ha na tho star tho da do yaar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+termux-setup-storage
+echo ""
+mkdir /$HOME/.termux
+echo "extra-keys = [['/','ls','$','~','UP','cd','*'],['exit','CTRL','ENTER','LEFT','DOWN','RIGHT','.']]" >> $HOME/.termux/termux.properties
+clear
+#maja aya mujha
+clear
+clear
+cd
