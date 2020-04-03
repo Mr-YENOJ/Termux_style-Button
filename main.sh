35f6f146800a311799bdf174f374860de6d99f30
Inderveerjoney <62381493+Inderveerjoney@users.noreply.github.com>
2020-04-03 13:46:56 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,6 +6,10 @@ echo -e write your cowsay name here..
 read kkcowsay
 echo -e write your next name here.. 
 read kktoilet
-echo "cowsay -f dragon "$kkcowsay" | lolcat" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-echo "toilet -f big "$kktoilet" | lolcat" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-cat "TSSB.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo "cowsay -f dragon "$kkcowsay" | lolcat" > cs.txt
+echo "toilet -f big "$kktoilet" | lolcat" > bn.txt
+echo ""
+cat "cs.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "bn.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "TSSB.txt"  >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo ""
