6c2c6f991aaca56b06521efa4b63310944396b01
Inderveerjoney <62381493+Inderveerjoney@users.noreply.github.com>
2020-04-03 13:34:31 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,11 @@
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e write your cowsay name here..
+read kkcowsay
+echo -e write your next name here.. 
+read kktoilet
+echo "cowsay -f dragon "$kkcowsay" | lolcat" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo "toilet -f big "$kktoilet" | lolcat" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "TSSB.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
